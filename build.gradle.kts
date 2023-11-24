7b316d14b5f542047ad6276d73dcaae9d83bb41a
Veniamin Viflyantsev <knok16@gmail.com>
2023-11-24 13:24:01 +0400

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "1.9.20"
+}
+
+group = "com.github.knok16"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(8)
+}
